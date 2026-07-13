ce9607da92da8854dc0ab09309e4f561419a04ee
Olena457 <olenademianenko42@gmail.com>
2026-07-13 17:38:26 +0300

    feat: corected styles product id

--- apps/frontend/src/components/navigation/DesktopNav.tsx
+++ apps/frontend/src/components/navigation/DesktopNav.tsx
@@ -1,42 +1,4 @@
-// import { Box } from "@mui/material";
-// import Link from "next/link";
 
-// export const DesktopNav = () => {
-//   return (
-//     <Box sx={{ display: { xs: "none", md: "flex" }, gap: 6, fontWeight: 800 }}>
-//       <Link
-//         href="/"
-//         style={{
-//           textDecoration: "none",
-//           color: "inherit",
-//           textTransform: "uppercase",
-//         }}
-//       >
-//        HOME
-//       </Link>
-//       <Link
-//         href="/products"
-//         style={{
-//           textDecoration: "none",
-//           color: "inherit",
-//           textTransform: "uppercase",
-//         }}
-//       >
-//        PRODUCTS
-//       </Link>
-//       <Link
-//         href="/review"
-//         style={{
-//           textDecoration: "none",
-//           color: "inherit",
-//           textTransform: "uppercase",
-//         }}
-//       >
-//        REVIEW
-//       </Link>
-//     </Box>
-//   );
-// };
 "use client";
 
 import { Box, IconButton, Typography } from "@mui/material";
