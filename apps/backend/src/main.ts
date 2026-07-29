b5cac3f60893e1697c34f2b2bc0b632aa72697cf
Olena457 <olenademianenko42@gmail.com>
2026-07-29 23:28:46 +0300

    chore: added new variable

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
